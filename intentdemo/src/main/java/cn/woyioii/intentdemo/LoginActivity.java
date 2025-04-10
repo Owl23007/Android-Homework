a556389e41c7d3a2fdcf0ffc53c9963028ba2119
Oii Woof <w15072362290@163.com>
2025-04-10 08:50:12 +0800

    feat(intentdemo): 添加定时启动和通知功能
    - 在 MainActivity 中添加定时启动功能，用户可以通过按钮设置闹钟
    - 在 LoginActivity 中添加拍照和选择图片功能
    - 添加通知权限请求和发送通知的功能- 更新布局文件，添加新的按钮和ImageView
    - 在 AndroidManifest 中添加 POST_NOTIFICATIONS 权限

--- intentdemo/src/main/java/cn/woyioii/intentdemo/LoginActivity.java
+++ intentdemo/src/main/java/cn/woyioii/intentdemo/LoginActivity.java
@@ -1,25 +1,58 @@
 package cn.woyioii.intentdemo;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.widget.Toolbar;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private Button btn;
     private EditText et_phone;
     private EditText et_password;
+    private ImageView photo;
+
+    ActivityResultLauncher<Intent> launcher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(), result -> {
+                if (result.getResultCode() == RESULT_OK) {
+                    result.getData();
+                    Log.e("LoginActivity", "Result OK");
+                } else {
+                    Log.e("LoginActivity", "Result not OK");
+                }
+            });
+
+    ActivityResultLauncher<String> launcher2 = registerForActivityResult(
+            new ActivityResultContracts.GetContent(), result -> {
+                if (result != null) {
+                    photo.setImageURI(result);
+                    Log.e("LoginActivity", "Image selected");
+                } else {
+                    Log.e("LoginActivity", "No image selected");
+                }
+            });
+
+    ActivityResultLauncher<Void> launcher3 = registerForActivityResult(
+            new ActivityResultContracts.TakePicturePreview(),
+            new ActivityResultCallback<Bitmap>(){
+                @Override
+                public void onActivityResult(Bitmap result) {
+                    //result为拍摄照片Bitmap格式
+                    photo.setImageBitmap(result);
+                }
+            });
 
     public void initView() {
         toolbar = findViewById(R.id.toolbar);
@@ -28,6 +61,7 @@ public class LoginActivity extends AppCompatActivity {
         btn = findViewById(R.id.login_btn);
         if (btn != null) {
             btn.setOnClickListener(new View.OnClickListener() {
+
                 @Override
                 public void onClick(View v) {
                     // 提交登录数据
@@ -39,12 +73,19 @@ public class LoginActivity extends AppCompatActivity {
 
                     intent.putExtras(bundle);
 
-                    startActivity(intent);
+                    launcher.launch(intent);
                 }
             });
         } else {
             Log.e("LoginActivity", "Button with id login_btn not found");
         }
+        photo = (ImageView) findViewById(R.id.photo);
+        photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launcher3.launch(null);
+            }
+        });
     }
 
     @Override
@@ -67,6 +108,16 @@ public class LoginActivity extends AppCompatActivity {
         } else {
             // Log 或者其他处理，表示 Toolbar 未找到
             Log.e("LoginActivity", "Toolbar not found");
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode ==200){
+            if (resultCode == RESULT_OK){
+                photo.setImageURI(data.getData());
+            }
         }
     }
 }
--- intentdemo/src/main/java/cn/woyioii/intentdemo/MainActivity.java
+++ intentdemo/src/main/java/cn/woyioii/intentdemo/MainActivity.java
@@ -1,14 +1,34 @@
 package cn.woyioii.intentdemo;
 
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     private Button btn1;
     private Button btn2;
+    private Button btn3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
     public void initView() {
         btn1 = findViewById(R.id.button1);
         btn2 = findViewById(R.id.button2);
+        btn3 = findViewById(R.id.button3);
+
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,8 +54,114 @@ public class MainActivity extends AppCompatActivity {
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 隐式intent测试
+//                Intent intent = new Intent();
+//                intent.setAction( Intent.ACTION_SEND );
+//                //下面2行代码功能：将项目raw文件夹中图片复制到手机相册，便于得到图片uri值
+//                Bitmap bitmap = BitmapFactory.decodeResource( getResources(), R.raw.gui );
+//                Uri uri = Uri.parse(MediaStore.Images.Media.insertImage(getContentResolver(),bitmap,"hi",""));
+//                intent.setType("image/*"); //设置数据MIME类型
+//
+//                intent.putExtra( Intent.EXTRA_STREAM, uri );
+//                startActivity(intent);
+//                // Intent.EXTRA_STREAM是附件
+                requestPermission();    //申请权限
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               testAlarm();
             }
         });
     }
-}
+
+    public void testAlarm() {
+        Calendar calendar = Calendar.getInstance();
+        TimePickerDialog tpd = new TimePickerDialog(
+                MainActivity.this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(android.widget.TimePicker view, int hourOfDay, int minute) {
+                        // ①创建Intent
+                        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                        // ②创建PendingIntent(封装intent)
+                        PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 100, intent, PendingIntent.FLAG_IMMUTABLE);
+
+                        Calendar tmp = Calendar.getInstance();
+                        tmp.set(Calendar.HOUR_OF_DAY, hourOfDay);  //设置时
+                        tmp.set(Calendar.MINUTE, minute);          //设置分
+                        tmp.set(Calendar.SECOND, 0);               //设置秒
+                        // ③创建闹钟（一次性任务）
+                        AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
+                        manager.set(AlarmManager.RTC_WAKEUP, tmp.getTimeInMillis(), pendingIntent);
+                        Toast.makeText(MainActivity.this, "闹钟设置成功", Toast.LENGTH_SHORT).show();
+                    }
+                },
+                calendar.get(Calendar.HOUR_OF_DAY),
+                calendar.get(Calendar.MINUTE),
+                true
+        );
+        tpd.show();
+    }
+
+    private static final int NOTIFY_ID = 1;
+    //通知的id (自拟)
+    private static final String CHANNEL_ID = "123";     //通知渠道的id (自拟)
+    private static final String CHANNEL_Name = "mychannel";    //通知渠道的名称(自拟)
+    NotificationManager manager;
+    Notification notification;
+    NotificationChannel channel;
+    PendingIntent pendingIntent;
+    Bitmap bitmap;
+    //通知管理类
+    //通知类
+    //通知渠道
+    //延迟Intent
+    //通知的LargeIcon
+    // (1) 创建权限申请的启动器
+    private final ActivityResultLauncher permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+                @Override
+                public void onActivityResult(Boolean result) {
+                    if (result) {    // true表示申请成功
+                        sendNotify();     //发送通知
+                    }
+                }
+            }
+
+    );
+
+    // (2) 在需要的时候启动权限请求
+    private void requestPermission() {
+
+        permissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS);
+    }
+
+    public void sendNotify() {
+        // ② 创建通知管理器
+        manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        // ③ 创建通知渠道
+        channel = new NotificationChannel(CHANNEL_ID, CHANNEL_Name,
+                NotificationManager.IMPORTANCE_HIGH);
+        manager.createNotificationChannel(channel);
+        // ④ 创建PendingIntent
+
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        pendingIntent = PendingIntent.getActivity(MainActivity.this, 0,
+                intent, PendingIntent.FLAG_IMMUTABLE);
+        // ⑤ 创建大图标的Bitmap
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.background);
+        notification = new Notification.Builder(MainActivity.this, CHANNEL_ID)
+                .setContentTitle("提醒")                //通知标题
+                .setContentText("通知内容…")     //通知内容
+                .setWhen(System.currentTimeMillis())   //通知产生的时间
+                .setShowWhen(true)     //显示时间
+                .setSmallIcon(R.drawable.icon)    //小图标
+                .setLargeIcon(bitmap)     //大图标Bitmap
+                .setAutoCancel(true)       //通知点击后自动删除
+                .setContentIntent(pendingIntent)    //设置通知点击的Intent
+                .build();     //构建通知
+        // ⑦发送通知
+        manager.notify(NOTIFY_ID, notification);
+    }
+    }
